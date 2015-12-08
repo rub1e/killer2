fddb094638f6f52e30a08f4db163dc255219e884
rub1e <alex.rubner@gmail.com>
2015-12-08 18:04:47 +0000

    eod

--- client/bodyEvents.js
+++ client/bodyEvents.js
@@ -2,5 +2,9 @@ Template.body.events({
   "click .modalButton": function(event, template){
     var modal = event.currentTarget.getAttribute("data-modal").replace("modal", "");
     Modal.show(modal);
+  },
+  "click": function(event, template){
+    var modal = event.currentTarget.getAttribute("data-modal");
+    console.log("click", modal);
   }
 });
