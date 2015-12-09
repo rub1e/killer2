21968ec3f60785bd8c4738a40ca6b28e580447d3
rub1e <alex.rubner@gmail.com>
2015-12-09 14:26:50 +0000

    finished login

--- client/bodyEvents.js
+++ client/bodyEvents.js
@@ -2,9 +2,5 @@ Template.body.events({
   "click .modalButton": function(event, template){
     var modal = event.currentTarget.getAttribute("data-modal").replace("modal", "");
     Modal.show(modal);
-  },
-  "click": function(event, template){
-    var modal = event.currentTarget.getAttribute("data-modal");
-    console.log("click", modal);
   }
 });
