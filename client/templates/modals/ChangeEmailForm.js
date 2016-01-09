9a466f2f220e69c3242e4180c5c104abb92c36ea
rub1e <alex.rubner@gmail.com>
2016-01-09 19:32:53 +0000

    abandoning list of emails for single email

--- client/templates/modals/ChangeEmailForm.js
+++ client/templates/modals/ChangeEmailForm.js
@@ -9,9 +9,23 @@ Template.ChangeEmailForm.events({
     var email2 = $("input[name=newEmail2]").val();
     // TODO: add validation to client AND server (send both to server) and figure out why standard HTML5 validation isn't working
     if(email1 === email2){
-      Meteor.call("changeEmailMethod", email1);
+      Meteor.call("changeEmailMethod", email1, email2);
     } else {
       alert("no match");
+    }
+  }
+});
+
+Template.ChangeEmailForm.helpers({
+   emailsArray : function(){
+    return Meteor.user().emails;
+  },
+  multiEmails : function () {
+    if(Meteor.user().emails.length === 1) {
+      return {
+        disabled : "",
+        class : "disabled"
+      };
     }
   }
 });
