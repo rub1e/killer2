966aaabb068c7e2ed24d246dc544344e10a84099
rub1e <alex.rubner@gmail.com>
2016-01-28 16:39:11 +0000

    mini league lives left working

--- client/templates/leagues/MiniActiveLeagueDetails.js
+++ client/templates/leagues/MiniActiveLeagueDetails.js
@@ -1,9 +1,16 @@
 Template.MiniActiveLeagueDetails.helpers({
-  playersLeft: function(){
+  playersLeft: function () {
     //return number of active players
   },
 
-  playersStarted: function() {
+  playersStarted: function () {
     //return length of players array
+  },
+
+  livesLeft : function () {
+    return this.members.filter(function(a) {
+      return a.playerId === Meteor.userId();
+    })[0].livesLeft;
   }
+  
 });
