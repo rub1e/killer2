cf3aa13f82fb2fa4ad96dd4de344d078f86b424d
rub1e <alex.rubner@gmail.com>
2016-01-29 14:45:07 +0000

    detailed modals working, pending league details done

--- client/templates/leagues/KillerAliveLeagues.js
+++ client/templates/leagues/KillerAliveLeagues.js
@@ -2,5 +2,14 @@ Template.KillerAliveLeagues.helpers({
 
   userAliveLeaguesList : function () {
     return Leagues.find({"members.playerId" : Meteor.userId()});
+  },
+
+});
+
+Template.KillerAliveLeagues.events({
+
+  "click .leagueDetailsButton" : function (event, template) {
+    var modal = event.currentTarget.getAttribute("data-modal").replace("modal", "");
+    Modal.show(modal, this);
   }
 });
