1b3cd0bf1ecad92eb025d9a816f2dd46b20da3b9
rub1e <alex.rubner@gmail.com>
2016-02-22 14:42:03 +0000

    fb sdk working

---
+++ app/client/socialInit.js
@@ -0,0 +1,10 @@
+
+//Initialise FB SDK
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : "436461956559485",
+    status     : true,
+    xfbml      : true,
+    version    : "v2.5"
+  });
+};
