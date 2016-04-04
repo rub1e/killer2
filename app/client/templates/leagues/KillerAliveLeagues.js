ffdd6ffa4c484765eaf1751443b0f2a40b7173c1
rub1e <alex.rubner@gmail.com>
2016-04-04 17:36:17 +0100

    it's fucking beautiful

--- app/client/templates/leagues/KillerAliveLeagues.js
+++ app/client/templates/leagues/KillerAliveLeagues.js
@@ -4,43 +4,10 @@ Template.KillerAliveLeagues.helpers({
 
 //LeaguePanels
 
-Template.LeaguePanel.events({
-
-  "click .show-hide-info" : function (event, template) {
-    var showHide = template.showHide.get() === "Show details" ? "Hide details" : "Show details";
-    template.showHide.set(showHide);
-  },
-
-});
-
-
-Template.LeaguePanel.onCreated(function () {
-  this.showHide = new ReactiveVar("Show details");
-});
-
 Template.LeaguePanel.helpers({
 
   playersStarted: function () {
     return this.members.length;
-  },
-
-  showHidePlayersChoices : function () {
-    return Template.instance().showHide.get();
-  },
-
-  memberName : function () {
-    var member;
-    var chairman = Template.parentData(1).members[0].playerId;
-    if (this.playerId === Meteor.userId()) {
-      member = "You";
-    } else {
-      member = Meteor.users.findOne(this.playerId).fullName();
-    }
-    if (this.playerId === chairman) {
-      return member + " (chairman)";
-    } else {
-      return member;
-    }
   },
 
   memberChoice : function () {
@@ -60,15 +27,6 @@ Template.LeaguePanel.helpers({
       return a - b;
     });
   },
-
-  isAutoPick : function () {
-    // because this is in an each block, go up one level to get league object
-    var league = Template.parentData(1);
-    var event = league.events[league.events.length - 1];
-    if(event && event.round === league.round && event.autoPicks) {
-      return event.autoPicks.indexOf(this.playerId) > -1;
-    }
-  }
 
 });
 
@@ -93,4 +51,50 @@ Template.MiniActiveLeagueDetails.helpers({
     return currentPlayer.diedInRound;
   }
 
+});
+
+// fullActiveLeagueDetails Template
+
+Template.FullActiveLeagueDetails.onCreated(function () {
+  this.showHide = new ReactiveVar("Show full details");
+});
+
+Template.FullActiveLeagueDetails.events({
+
+  "click .show-hide-info" : function (event, template) {
+    var showHide = template.showHide.get() === "Show full details" ? "Hide full details" : "Show full details";
+    template.showHide.set(showHide);
+  },
+
+});
+
+Template.FullActiveLeagueDetails.helpers({
+
+  memberName : function () {
+    var member;
+    var chairman = Template.parentData(1).members[0].playerId;
+    if (this.playerId === Meteor.userId()) {
+      member = "You";
+    } else {
+      member = Meteor.users.findOne(this.playerId).fullName();
+    }
+    if (this.playerId === chairman) {
+      return member + " (chairman)";
+    } else {
+      return member;
+    }
+  },
+
+  showHidePlayersChoices : function () {
+    return Template.instance().showHide.get();
+  },
+
+  isAutoPick : function () {
+    // because this is in an each block, go up one level to get league object
+    var league = Template.parentData(1);
+    var event = league.events[league.events.length - 1];
+    if(event && event.round === league.round && event.autoPicks) {
+      return event.autoPicks.indexOf(this.playerId) > -1;
+    }
+  }
 });
