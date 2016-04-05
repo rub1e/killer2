08b9857048ce362355ea8f4977c3e69104df76f7
rub1e <alex.rubner@gmail.com>
2016-04-05 12:08:09 +0100

    added blank to email and moved panel helpers to full details

--- app/client/templates/leagues/KillerAliveLeagues.js
+++ app/client/templates/leagues/KillerAliveLeagues.js
@@ -6,27 +6,7 @@ Template.KillerAliveLeagues.helpers({
 
 Template.LeaguePanel.helpers({
 
-  playersStarted: function () {
-    return this.members.length;
-  },
 
-  memberChoice : function () {
-    return this.picks[this.picks.length - 1];
-  },
-
-  aliveMembers : function () {
-    return Template.currentData().members.filter(function (a) {
-      return a.diedInRound === 0;
-    });
-  },
-
-  deadMembers : function () {
-    return Template.currentData().members.filter(function (a) {
-      return a.diedInRound !== 0;
-    }).sort(function (a, b) {
-      return a - b;
-    });
-  },
 
 });
 
@@ -96,5 +76,28 @@ Template.FullActiveLeagueDetails.helpers({
     if(event && event.round === league.round && event.autoPicks) {
       return event.autoPicks.indexOf(this.playerId) > -1;
     }
+  },
+
+  playersStarted: function () {
+    return this.members.length;
+  },
+
+  memberChoice : function () {
+    return this.picks[this.picks.length - 1];
+  },
+
+  aliveMembers : function () {
+    return Template.currentData().members.filter(function (a) {
+      return a.diedInRound === 0;
+    });
+  },
+
+  deadMembers : function () {
+    return Template.currentData().members.filter(function (a) {
+      return a.diedInRound !== 0;
+    }).sort(function (a, b) {
+      return a - b;
+    });
   }
+  
 });
