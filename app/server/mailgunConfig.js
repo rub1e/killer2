2b655fa953bee0e72f98a3753aea45c54ca13a4a
rub1e <alex.rubner@gmail.com>
2016-04-08 08:35:09 +0100

    saving

---
+++ app/server/mailgunConfig.js
@@ -0,0 +1,8 @@
+Meteor.startup(function(){
+  // process.env.MAIL_URL="smtp://postmaster@sandboxe308f129bdb647b4948e7d35c8687980.mailgun.org:e016c6fe2316f8ee2c82e8a8eae473d5@smtp.mailgun.org:587";
+  process.env.MAIL_URL="smtp://postmaster@killer.football:9a88bd540bb57456bdfa9d7410899229@smtp.mailgun.org:587";
+// Accounts.emailTemplates.from = "killerDOTfootball <chairman@killer.football>";
+// Accounts.emailTemplates.siteName = "killer.football";
+// Accounts.emailTemplates.headers = {replyTo : "andy@killer.football"};
+});
+// FIXME: env variables
