ad1f308fa776531f1ce40abb32c8fc4ccc2f4d4f
rub1e <alex.rubner@gmail.com>
2016-04-11 17:27:10 +0100

    added this ready to pubs

--- app/server/pubs.js
+++ app/server/pubs.js
@@ -4,5 +4,11 @@ Meteor.publish("allMatches", function(argument){
   var admin = Roles.userIsInRole(this.userId, "admin");
   var users = admin ? Meteor.users.find() : Meteor.users.find({}, {fields : {_id : 1, "profile.firstName" : 1, "profile.lastName" : 1}});
   var data = [Matches.find(), GameStatus.find(), Leagues.find({}), users];
-  return data;
+
+  if(data) {
+    return data;
+  }
+
+  return this.ready();
+
 });
