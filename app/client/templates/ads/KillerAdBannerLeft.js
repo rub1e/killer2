8cfd8eb74a20ef086ae87520b48c512193288ff5
rub1e <alex.rubner@gmail.com>
2016-04-11 15:53:00 +0100

    all ads present and correct

--- app/client/templates/ads/KillerAdBannerLeft.js
+++ app/client/templates/ads/KillerAdBannerLeft.js
@@ -1,8 +1,9 @@
-// Template.KillerAdBannerLeft.rendered = function() {
-//   $.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
-//     var ads, adsbygoogle;
-//     ads = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6892043911817762" data-ad-slot="1431309130" data-ad-format="auto"></ins>';
-//     $('#KillerAdBannerLeft').html(ads);
-//     return (adsbygoogle = window.adsbygoogle || []).push({});
-//   });
-// };
+Template.KillerAdBannerLeft.rendered = function() {
+  $.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
+    var ads, adsbygoogle;
+    ads = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6892043911817762" data-ad-slot="1431309130" data-ad-format="auto"></ins>';
+    $('#KillerAdBannerLeft').html(ads);
+    return (adsbygoogle = window.adsbygoogle || []).push({});
+  });
+  console.log("left rendered");
+};
--- app/client/templates/ads/KillerAdBannerTop.js
+++ app/client/templates/ads/KillerAdBannerTop.js
@@ -1,7 +1,7 @@
 Template.KillerAdBannerTop.rendered = function() {
   $.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
     var ads, adsbygoogle;
-    ads = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6892043911817762" data-ad-slot="1431309130" data-ad-format="auto"></ins>';
+    ads = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6892043911817762" data-ad-slot="6500637134" data-ad-format="auto"></ins>';
     $('#KillerAdBannerTop').html(ads);
     return (adsbygoogle = window.adsbygoogle || []).push({});
   });
