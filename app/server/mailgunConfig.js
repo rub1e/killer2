d719a5a34ee002b7780bea7f2e37f7f6fc29e82e
rub1e <alex.rubner@gmail.com>
2016-04-11 09:35:49 +0100

    merged mail and between

--- app/server/mailgunConfig.js
+++ app/server/mailgunConfig.js
@@ -1,11 +1,5 @@
 Meteor.startup(function(){
-<<<<<<< HEAD
-  // process.env.MAIL_URL="smtp://postmaster@sandboxe308f129bdb647b4948e7d35c8687980.mailgun.org:e016c6fe2316f8ee2c82e8a8eae473d5@smtp.mailgun.org:587";
-  process.env.MAIL_URL="smtp://postmaster@killer.football:9a88bd540bb57456bdfa9d7410899229@smtp.mailgun.org:587";
-// Accounts.emailTemplates.from = "killerDOTfootball <chairman@killer.football>";
-// Accounts.emailTemplates.siteName = "killer.football";
-// Accounts.emailTemplates.headers = {replyTo : "andy@killer.football"};
-=======
+  // process.env.MAIL_URL="smtp://postmaster@killer.football:9a88bd540bb57456bdfa9d7410899229@smtp.mailgun.org:587";
 
   process.env.MAIL_URL = "smtp://postmaster@sandboxe308f129bdb647b4948e7d35c8687980.mailgun.org:e016c6fe2316f8ee2c82e8a8eae473d5@smtp.mailgun.org:587";
 
@@ -21,7 +15,4 @@ Meteor.startup(function(){
     console.log(url);
     return "Welcome to killerDOTfootball! Please verify your email by clicking the link below: \n\n" + url + "\n\n And if you ever need any assistance, just email chairman@killer.football or reply to this message\n\n\ Good luck!";
   };
-
->>>>>>> a252a59738c3be049cafffdb954f6eb27f1c039d
 });
-// FIXME: env variables
